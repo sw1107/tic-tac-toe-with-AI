93461b438e2fd260ef335e3100b82da1eb485b0a
sophiewhittle <sophiewhittle89@gmail.com>
2022-03-10 14:28:24 -0500

    add tkinter GUI, add README

--- main.py
+++ main.py
@@ -1,18 +1,34 @@
 from copy import deepcopy
 from itertools import cycle
 from random import randint
+from tkinter import *
+from tkinter import messagebox
+from functools import partial
 
+# ---------------------- CONSTANTS ----------------------
+FONT_NAME = "Courier"
+BACKGROUND_COLOUR = "#011CFF"
+FONT_COLOUR = "white"
+BOX_WIDTH = 2
+BOX_HEIGHT = 1
+BOX_FONT_SIZE = 150
+TOP_LABEL_FONT_SIZE = 15
+
+# ---------------------- GAME VARIABLES ----------------------
 BOARD = {}
 player = cycle(range(2))
 
 
-def main():
-    while input("Do you want to play a game of Tic Tac Toe? Type 'y' to play': ") == 'y':
-        print_template()
-        play_game()
+# ---------------------- GUI FUNCTIONS ----------------------
+def make_move(box):
+    if BOARD[box] == " ":
+        player_move.set(box)
+    else:
+        messagebox.showwarning(title="Move invalid", message="That move is not available, please try again")
 
 
 def play_game():
+
     reset_board()
     number_of_moves = 0
     is_game_over = False
@@ -21,45 +37,37 @@ def play_game():
     for _ in range(randint(1, 2)):
         current_player = next(player)
 
+    if current_player == 0:
+        top_label.config(text="Computer starts")
+    else:
+        top_label.config(text="Player starts")
+
     while not is_game_over:
 
         if current_player == 0:
-            if number_of_moves == 0:
-                print("Computer thinking...")
             current_board = deepcopy(BOARD)
             next_move = minimax(current_board, current_player)[1]
             if BOARD[next_move] != " ":
-                print(f"Computer move invalid: {next_move}")
+                messagebox.showwarning(title="Computer error", message="Computer made an invalid move")
                 return
-            else:
-                print(f"Computer move: {next_move + 1}")
         else:
-            while True:
-                next_move = input(f"Player move? Type 1-9: ")
-                try:
-                    next_move = int(next_move)
-                except ValueError:
-                    print("Move not valid")
-                    pass
-                if next_move in range(1, 10):
-                    if BOARD[next_move - 1] == " ":
-                        break
-                    else:
-                        print("Move not available")
-                else:
-                    print("Move not valid")
+            window.wait_variable(player_move)
+            next_move = player_move.get()
 
         # update board with move
         if current_player == 0:
             BOARD[next_move] = 'x'
+            all_buttons[next_move].config(text="X")
         else:
-            BOARD[next_move - 1] = 'o'
+            BOARD[next_move] = 'o'
+            all_buttons[next_move].config(text="O")
+
+        if number_of_moves == 1:
+            top_label.config(text="")
 
         # toggle to next player
         current_player = next(player)
         number_of_moves += 1
-
-        print_board()
 
         # check for game over
         if number_of_moves >= 5:
@@ -68,13 +76,14 @@ def play_game():
     # compute value of final board and declare winner
     final_board_value = value_board(BOARD)
     if final_board_value == 1:
-        print("Game over: Computer is the winner! \n")
+        top_label.config(text="Game over: Computer is the winner!")
     elif final_board_value == -1:
-        print("Game over: Player is the winner! \n")
+        top_label.config(text="Game over: Player is the winner!")
     else:
-        print("Game over: No winner\n")
+        top_label.config(text="Game over: No winner")
 
 
+# ---------------------- TIC TAC TOE FUNCTIONS ----------------------
 def check_game_over(current_board):
     # TODO: improve check_game_over function
     if ((current_board[0] == current_board[1] == current_board[2] and current_board[0] != " ") or
@@ -95,6 +104,8 @@ def check_game_over(current_board):
 def reset_board():
     for i in range(9):
         BOARD[i] = " "
+    for button in all_buttons:
+        button.config(text="")
 
 
 def print_board():
@@ -188,5 +199,56 @@ def minimax(current_board, current_player):
     return value, best_move
 
 
-if __name__ == "__main__":
-    main()
+# ---------------------- GUI ----------------------
+window = Tk()
+window.title("Tic Tac Toe")
+window.config(padx=50, pady=50)
+
+top_label = Label(text="Press start to play!", fg=BACKGROUND_COLOUR, font=(FONT_NAME, TOP_LABEL_FONT_SIZE))
+top_label.grid(column=0, row=0, columnspan=2)
+
+start_button = Button(text="Start", width=10, height=2, highlightbackground=BACKGROUND_COLOUR, fg=FONT_COLOUR,
+                      font=FONT_NAME, command=play_game)
+start_button.grid(column=2, row=0, padx=10, pady=10)
+
+player_move = IntVar()
+
+all_buttons = []
+b0 = Button(text="", width=BOX_WIDTH, height=BOX_HEIGHT, highlightbackground=BACKGROUND_COLOUR, fg=FONT_COLOUR,
+            font=(FONT_NAME, BOX_FONT_SIZE), command=partial(make_move, 0))
+b0.grid(column=0, row=1)
+all_buttons.append(b0)
+b1 = Button(text="", width=BOX_WIDTH, height=BOX_HEIGHT, highlightbackground=BACKGROUND_COLOUR, fg=FONT_COLOUR,
+            font=(FONT_NAME, BOX_FONT_SIZE), command=partial(make_move, 1))
+b1.grid(column=1, row=1)
+all_buttons.append(b1)
+b2 = Button(text="", width=BOX_WIDTH, height=BOX_HEIGHT, highlightbackground=BACKGROUND_COLOUR, fg=FONT_COLOUR,
+            font=(FONT_NAME, BOX_FONT_SIZE), command=partial(make_move, 2))
+b2.grid(column=2, row=1)
+all_buttons.append(b2)
+b3 = Button(text="", width=BOX_WIDTH, height=BOX_HEIGHT, highlightbackground=BACKGROUND_COLOUR, fg=FONT_COLOUR,
+            font=(FONT_NAME, BOX_FONT_SIZE), command=partial(make_move, 3))
+b3.grid(column=0, row=2)
+all_buttons.append(b3)
+b4 = Button(text="", width=BOX_WIDTH, height=BOX_HEIGHT, highlightbackground=BACKGROUND_COLOUR, fg=FONT_COLOUR,
+            font=(FONT_NAME, BOX_FONT_SIZE), command=partial(make_move, 4))
+b4.grid(column=1, row=2)
+all_buttons.append(b4)
+b5 = Button(text="", width=BOX_WIDTH, height=BOX_HEIGHT, highlightbackground=BACKGROUND_COLOUR, fg=FONT_COLOUR,
+            font=(FONT_NAME, BOX_FONT_SIZE), command=partial(make_move, 5))
+b5.grid(column=2, row=2)
+all_buttons.append(b5)
+b6 = Button(text="", width=BOX_WIDTH, height=BOX_HEIGHT, highlightbackground=BACKGROUND_COLOUR, fg=FONT_COLOUR,
+            font=(FONT_NAME, BOX_FONT_SIZE), command=partial(make_move, 6))
+b6.grid(column=0, row=3)
+all_buttons.append(b6)
+b7 = Button(text="", width=BOX_WIDTH, height=BOX_HEIGHT, highlightbackground=BACKGROUND_COLOUR, fg=FONT_COLOUR,
+            font=(FONT_NAME, BOX_FONT_SIZE), command=partial(make_move, 7))
+b7.grid(column=1, row=3)
+all_buttons.append(b7)
+b8 = Button(text="", width=BOX_WIDTH, height=BOX_HEIGHT, highlightbackground=BACKGROUND_COLOUR, fg=FONT_COLOUR,
+            font=(FONT_NAME, BOX_FONT_SIZE), command=partial(make_move, 8))
+b8.grid(column=2, row=3)
+all_buttons.append(b8)
+
+window.mainloop()
